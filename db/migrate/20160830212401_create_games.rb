61ac8bacfc7a4b921d3064724aa4145e3df42c62
thotmx <hermes.o.r@gmail.com>
2016-08-30 18:43:17 -0500

    Remove constrait to fit heroku

--- db/migrate/20160830212401_create_games.rb
+++ db/migrate/20160830212401_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.references :player, index: true, foreign_key: true
-      t.references :opponent, index: true, foreign_key: true
+      t.references :player, index: true
+      t.references :opponent, index: true
       t.date :date_played
       t.integer :player_score
       t.integer :opponent_score
